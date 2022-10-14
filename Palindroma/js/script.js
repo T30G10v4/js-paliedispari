2498a4cb2ac551c2c4e0a35aaecdfaaf29925680
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-10-14 14:25:04 +0200

    BUG - Somma +1 all'indice j

---
+++ Palindroma/js/script.js
@@ -0,0 +1,50 @@
+let choice = prompt("Inserisci una parola a tua scelta");
+
+console.log(choice);
+
+let result = "";
+
+if(isPalindroma(choice)) {
+
+    result = `La parola da te scelta è palindroma`;
+
+} else {
+
+    result = "La parola da te scelta NON è palindroma";
+
+}
+
+console.log(result);
+
+// FUNCTION oddEven
+function isPalindroma (word) {
+
+    let i = 0;
+    let j = word.length-1;
+
+    console.log("Contatore I", i , "Contantore J", j);
+
+    for(; i < word.length; i++) {
+
+        console.log("Contatore I", i , "Contantore J", j);
+
+        while(j >= 0) {
+            
+            console.log("Contatore I", i , "Contantore J", j);
+                
+            if(word[i] !== word[j]) { 
+
+                console.log("Contatore I", i , "Contantore J", j);
+                
+                return false;
+            
+            }
+
+            j--;
+
+        }
+    }
+    
+    return true;
+
+}
